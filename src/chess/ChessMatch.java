8e79d7416095e7895a98f2bfce4c780ddd3b5ee8
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-11-18 20:53:37 -0300

    Moving pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -23,6 +25,27 @@ public class ChessMatch {
 		}
 
 		return mat;
+	}
+
+	public ChessPiece performChessMove(ChessPosition sourcePostion, ChessPosition targetPosition) {
+		Position source = sourcePostion.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece) capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position targert) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(targert);
+		board.placePiece(p, targert);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
